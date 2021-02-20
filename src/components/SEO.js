d7d22efb13e49f90291345a1afb0a5b0257a6aa4
Shuin <garlen.javier@gmail.com>
2021-02-21 06:32:48 +0800

    - modify seo

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -21,8 +21,8 @@ const SEO = ({ title, description, image, article }) => {
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
-    //image: `${siteUrl}${image || defaultImage}`,
-    image: `${siteUrl}${image}` || defaultImage,
+    image: `${siteUrl}${image || defaultImage}`,
+    //image: `${siteUrl}${image}` || defaultImage,
     url: siteUrl,
   }
 
